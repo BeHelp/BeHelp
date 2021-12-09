b746bf3a0ae94e70a95bc5e524eb94a26510913b
danielhalasz <halaszdaniel@gmail.com>
2021-12-10 00:56:55 +0100

    getUser filtering should work

--- backend/business-logic/users.js
+++ backend/business-logic/users.js
@@ -19,11 +19,42 @@ const userManager = {
   },
   getUser: async (userData) => {
     try {
-      const user = await User.find({
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        // volunteer: userData.volunteer,
-      });
+      const searchQuery = {};
+
+      if (
+        userData.location !== '' &&
+        userData.location !== undefined &&
+        userData.location !== null &&
+        userData.location !== []
+      ) {
+        searchQuery.location = userData.location;
+      }
+      if (
+        userData.languages !== '' &&
+        userData.languages !== undefined &&
+        userData.languages !== null &&
+        userData.languages !== []
+      ) {
+        searchQuery.languages = userData.languages;
+      }
+      if (
+        userData.skills !== '' &&
+        userData.skills !== undefined &&
+        userData.skills !== null &&
+        userData.skills !== []
+      ) {
+        searchQuery.skills = userData.skills;
+      }
+      if (
+        userData.firstName !== '' &&
+        userData.firstName !== undefined &&
+        userData.firstName !== null
+      ) {
+        searchQuery.firstName = userData.firstName;
+      }
+
+      const user = await User.find(searchQuery);
+
       console.log(user);
       return user;
       // mongoose.connection.close();
