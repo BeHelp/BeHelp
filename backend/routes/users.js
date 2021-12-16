dde592bc7a0d8ad3b584cd5188e71278e035cb9f
danielhalasz <halaszdaniel@gmail.com>
2021-12-16 13:22:21 +0100

    getbyid added and delete route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/users');
 userRoutes.post('/', userController.post);
 userRoutes.get('/', userController.get);
 userRoutes.get('/volunteers', userController.getAll);
-userRoutes.get('/:userId', userController.get);
+userRoutes.get('/:userId', userController.getbyid);
+userRoutes.delete('/:userId', userController.delete);
 
 module.exports = userRoutes;
