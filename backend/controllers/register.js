80058a68472a78cc0940a2708a28bdd57088ee7d
katsmamina <katsmamina@yandex.ru>
2021-12-17 17:18:55 +0300

    deleted extra lines

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -32,18 +32,6 @@ module.exports = registerController = {
             res.status(500).send(error);
         }
     },
-    // post: async (req, res) => {
-    //     try {
-    //         const userData = req.body;
-    //         console.log(userData);
-    //         const result = await userManager.postUser(userData);
-    //         // res.status(201).send(result);
-    //         console.log(result);
-    //         res.status(201).send(`New user was successfully created!`);
-    //     } catch (error) {
-    //         res.status(500).send(error);
-    //     }
-    // }
 }
 
 module.exports = registerController;
