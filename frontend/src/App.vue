8c773dc7ee8407e020e62cf5e53fb36a46dfba80
danielhalasz <halaszdaniel@gmail.com>
2021-12-18 17:18:21 +0100

    migrated repo to Vue for testing

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
