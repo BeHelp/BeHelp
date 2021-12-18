99bf4dc27a3c80e5e5ba5148152b058de67704d8
danielhalasz <halaszdaniel@gmail.com>
2021-12-18 17:47:17 +0100

    migrated repo to Vue for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Header from './components/Header.vue';
+import About from './components/About.vue';
 </script>
 
 <template>
-  <Header />
+  <div class="app">
+    <Header />
+    <About />
+  </div>
 </template>
 
 <style lang="scss">
