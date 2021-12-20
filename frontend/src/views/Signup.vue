62aa3df50d615d7ea939939cff9d2979fda253f4
danielhalasz <halaszdaniel@gmail.com>
2021-12-20 12:28:46 +0100

    cloudinary function added to Signup.vue

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,6 +1,25 @@
+<script>
+export default {
+  mounted() {
+    const plugin = document.createElement('script');
+    plugin.setAttribute('src', '../src/assets/js/cloudinary.js');
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  },
+};
+</script>
+
 <template>
   <div>
     <h1>Signup</h1>
     <p>Signup part of the application</p>
+    <div>
+      <form method="post" enctype="multipart/form-data">
+        <input type="file" name="file" />
+        <input type="submit" value="Upload File" name="submit" />
+      </form>
+
+      <p id="data"></p>
+    </div>
   </div>
 </template>
