86ae6073d3638deb80ec0a2639f16a5cdf0893a1
danielhalasz <halaszdaniel@gmail.com>
2021-12-30 13:46:13 +0100

    user email extracted from JWT header after successful login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -19,12 +19,13 @@
           placeholder="Password"
           autocomplete="off"
           required
-        /><br>
+        /><br />
         <button class="login__btn" @click="login" type="submit">LOG IN</button>
 
         <p class="p1">
           You don't have an account?
-          <a class="signup__text"
+          <a
+            class="signup__text"
             href="/Signup"
             @click="(registerActive = !registerActive), (emptyFields = false)"
             >Sign Up</a
@@ -32,38 +33,44 @@
         </p>
         <p class="p2"><a href="#">forgot your password?</a></p>
       </form>
-      
     </div>
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
-import { useRouter } from "vue-router";
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import VueJwtDecode from 'vue-jwt-decode';
 export default {
-  name: "Login",
+  name: 'Login',
+  data() {
+    return {
+      user: {},
+    };
+  },
   setup() {
     const data = reactive({
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     });
     const router = useRouter();
     const submit = async () => {
-      const response = await fetch("http://localhost:5000/login", {
-        method: "POST",
+      const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
+          user: data.email,
+          pass: data.password,
         },
-        body: JSON.stringify({
-          email: data.email,
-          password: data.password,
-        }),
       });
+      const token = response.headers.get('Authorization').split(' ')[1];
+      const userEmail = VueJwtDecode.decode(token).user.email;
+      console.log(userEmail);
       if (!response.ok) {
-        alert("password or email is wrong");
+        alert('password or email is wrong');
         throw new Error(`HTTP error! status:${response.status}`);
       }
-      await router.push("/volunteers");
+      await router.push('/volunteers');
     };
     return {
       data,
@@ -74,6 +81,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../components/styles/abstract/_variables.scss";
-@import "../components/styles/layout/_signin.scss";
+@import '../components/styles/abstract/_variables.scss';
+@import '../components/styles/layout/_signin.scss';
 </style>
