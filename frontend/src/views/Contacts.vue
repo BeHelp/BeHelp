75b5990603204477ba30eb0d0a2a763fdca1bdf9
TatsianaRud <tatsianaRud86@gmail.com>
2021-12-30 16:14:54 +0100

    firstVersionContactForm

--- frontend/src/views/Contacts.vue
+++ frontend/src/views/Contacts.vue
@@ -1,6 +1,72 @@
+
 <template>
-  <div>
-    <h1>Contact</h1>
-    <p>Contacts part of the application</p>
-  </div>
+ 
+    <div class="container">
+     
+        <form @submit.prevent="sendEmail">
+          <h1>CONTACT US</h1>
+          <label>Name</label>
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+    </div>
 </template>
+
+<script>
+import emailjs from 'emailjs-com';
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('service_lzyi0s6', 'template_y7zrvwv', e.target, 'user_6RSzucdIUsj0oQzdwj3VZ', {
+          name: this.name,
+          email: this.email,
+          message: this.message
+        })
+        console.log('it works!!!')
+      } catch (error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import '../components/styles/abstract/_variables.scss';
+@import '../components/styles/layout/_contact.scss';
+
+</style>
