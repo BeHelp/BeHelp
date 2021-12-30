ebd59f0f8f029c1d10f5e4af9e65f39192337783
katsmamina <katsmamina@yandex.ru>
2021-12-30 23:33:24 +0300

    improved registr form

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -39,7 +39,7 @@
                         </div>
 
                                 <div class="container__radio">
-                                    <label for="gender" class="radio-label">Status*</label>
+                                    <label for="usertype" class="radio-label">Status*</label>
                                     <div class="container__radio-item1">
                                         <input type="radio" name="status" id="newcomer" checked>
                                         <label for="newcomer">Newcomer</label>
@@ -51,52 +51,44 @@
                                         <label for="volunteer">Volunteer</label>
                                         <span class="container__radio-check"></span>
                                 </div>
-                                
-                                </div>
+                            </div>
+
                             <div class="container__group">
-                                <input type="email" placeholder="E-mail*" name="email" id="email" required />
+                                <input type="email" placeholder="E-mail*" name="email" id="email" required/>
                             </div>
                             <div class="container__group">
                                 <input type="text" placeholder="Password*" name="password" id="password" required>
                             </div>
-                            
-                            <div class="container__radio">
-                                <label for="gender" class="container__radio-label">Gender:</label>
-                                <div class="container__radio-item">
-                                    <input type="radio" name="gender" id="male" checked>
-                                    <label for="male">Male</label>
-                                    <span class="container__radio-check"></span>
-                                </div>
-                                <div class="container__radio-item">
-                                    <input type="radio" name="gender" id="female">
-                                    <label for="female">Female</label>
-                                    <span class="container__radio-check"></span>
-                                </div>
-                                
-                                <div class="container__radio-item">
-                                    <input type="radio" name="gender" id="other">
-                                    <label for="other">Other</label>
-                                    <span class="container__radio-check"></span>
+
+                            <div class="container__group">
+                            <div class="container__select">
+                                    <select name="Gender" id="gender" required>
+                                        <option value="Gender" disabled selected>Gender*</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                        <option value="Other">Other</option>
+                                    </select>
+                                    <span class="container__select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
                                 </div>
                             </div>
                         
                             <div class="container__group">
                             <input type="text" placeholder="Nationality" name="nationality" id="nationality"/>
                             </div>
+
                             <div class="container__group">
-                            
                                 <div class="container__select">
                                     <select name="languages" id="languages">
                                         <option value="Language" disabled selected>Languages</option>
                                         <option value="French">English</option>
+                                        <option value="Dutch">Dutch</option>
                                         <option value="English">French</option>
-                                        <option value="Turkish">Russian</option>
-                                        <option value="Turkish">Spanish</option>
-                                        <option value="Turkish">Dutch</option>
-                                        <option value="Turkish">Arabic</option>
-                                        <option value="Turkish">Hindu</option>
-                                        <option value="Turkish">Chinese</option>
-                                        <option value="Turkish">Italian</option>
+                                        <option value="Russian">Russian</option>
+                                        <option value="Spanish">Spanish</option>
+                                        <option value="Arabic">Arabic</option>
+                                        <option value="Hindu">Hindu</option>
+                                        <option value="Chinese">Chinese</option>
+                                        <option value="Italian">Italian</option>
                                     </select>
                                     <span class="container__select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
                                 </div>
@@ -119,8 +111,6 @@
                             </div>
                         
                                 <textarea class="container__description" placeholder="Description"></textarea>
-                        
-
                         
                             <div class="container__submit">
                                 <input type="submit" value="SEND" class="submit" name="submit" id="submit" />
