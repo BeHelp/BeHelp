7e0a01ecf730ffd538847d70e64f14b92b2bb11a
danielhalasz <halaszdaniel@gmail.com>
2022-01-02 15:24:55 +0100

    fontawesome and Vue store added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
+import { FontAwesomeIcon } from './assets/js/font-awesome';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
