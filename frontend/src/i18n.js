b26401a8bf2a7e92d7533b5b52a0e39ded13f1ca
danielhalasz <halaszdaniel@gmail.com>
2022-01-06 19:22:51 +0100

    translating header to FR & NL

---
+++ frontend/src/i18n.js
@@ -0,0 +1,25 @@
+import { createI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "ENG",
+  globalInjection: true,
+  messages: {
+    ENG: {
+      about: "About",
+      volunteers: "Volunteers",
+      contactus: "Contact Us",
+    },
+    FR: {
+      about: "À propos de nous",
+      volunteers: "Bénévoles",
+      contactus: "Contactez nous",
+    },
+    NL: {
+      about: "Over ons",
+      volunteers: "Vrijwilligers",
+      contactus: "Contacteer Ons",
+    },
+  },
+});
+export default i18n;
