6f7dcb8ae2899028c2640e88f460877727f3ef4a
danielhalasz <halaszdaniel@gmail.com>
2022-01-06 16:04:06 +0100

    centering & solving merge conflicts

--- frontend/src/views/Thankyou.vue
+++ frontend/src/views/Thankyou.vue
@@ -1,23 +1,24 @@
 <template>
-   <div class="thankyou">
-      <div class="thankyou__logo">
-        <img src="../assets/logos/Logo_small_orange.svg" alt="logo" />
-      </div>
-      <div class="thankyou__msg">
-        <h2 class="thankyou__msg-hdr">Thank You!</h2>
-        <p class="thankyou__msg-txt">
-          We have received your request. a volunteer will contact you as soon as
-          possible.
-        </p>
-      </div>
-      <div class="thankyou__wraper">
-        <!-- href link replaced with router-view -->
-        <router-link to="/" > <button class="thankyou__wraper-btn">Main Page</button></router-link>
-      </div>
+  <div class="thankyou">
+    <div class="thankyou__logo">
+      <img src="../assets/logos/Logo_small_orange.svg" alt="logo" />
     </div>
+    <div class="thankyou__msg">
+      <h2 class="thankyou__msg-hdr">Thank You!</h2>
+      <p class="thankyou__msg-txt">
+        We have received your request. <br />A volunteer will contact you
+        <br />as soon as possible.
+      </p>
+    </div>
+    <div class="thankyou__wrapper">
+      <!-- href link replaced with router-view -->
+      <router-link to="/">
+        <button class="thankyou__wrapper-btn">Main Page</button></router-link
+      >
+    </div>
+  </div>
 </template>
 <style lang="scss">
-@import '../components/styles/abstract/_variables.scss';
-@import '../components/styles/layout/_thankyou.scss';
+@import "../components/styles/abstract/_variables.scss";
+@import "../components/styles/layout/_thankyou.scss";
 </style>
-
