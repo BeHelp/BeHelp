f0e8ba2b939b93ca6247ef42d0919a7d38e765a1
danielhalasz <halaszdaniel@gmail.com>
2022-01-07 15:32:34 +0100

    styling moved to _base.scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,36 +18,4 @@ import Footer from "./components/Footer.vue";
 
 <style lang="scss">
 @import "../src/components/styles/style.scss";
-
-.my-notification {
-  // styling
-  border-radius: 20px;
-  margin: 20px 5px 5px;
-  padding: 20px;
-  font-size: 12px;
-  color: #ffffff;
-
-  // default (blue)
-  background: #44a4fc;
-  border-left: 5px solid #187fe7;
-
-  // types (green, amber, red)
-  &.success {
-    background: #68cd86;
-    border-left-color: #42a85f;
-    text-align: center;
-  }
-
-  &.warn {
-    background: #ffb648;
-    border-left-color: #f48a06;
-    text-align: center;
-  }
-
-  &.error {
-    background: #e54d42;
-    border-left-color: #b82e24;
-    text-align: center;
-  }
-}
 </style>
