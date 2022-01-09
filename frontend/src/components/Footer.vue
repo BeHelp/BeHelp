bdb2cef27c918ff48819981726d56ca58d50b70b
danielhalasz <halaszdaniel@gmail.com>
2022-01-09 23:18:41 +0100

    responsive footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,18 +1,49 @@
 <template>
+  <footer class="footer">
+    <div class="footer__logos">
+      <img
+        class="footer__logos-behelp"
+        src="../assets/logos/Logo_wb_orange.svg"
+        alt="logo"
+        height="53.02px"
+      />
+      <div>
+        <img
+          class="footer__logos-hyf"
+          src="../assets/hyf_logo.png"
+          alt="HYF"
+          height="53.02px"
+        />
+      </div>
+    </div>
+
+    <div class="footer__text">
+      <p class="footer__text-p">
+        Project from the students of HYF <br />
+        info@behelp.be <br />
+        1000 Bruxelles
+      </p>
+    </div>
+    <div class="footer__icons">
+      <a href="#"><img src="../assets/fas/facebook.svg" class="fa" /></a>
+      <a href="#"><img src="../assets/fas/twitter.svg" class="fa" /></a>
+      <a href="#"><img src="../assets/fas/instagram.svg" class="fa" /></a>
+    </div>
+  </footer>
   <div>
-    <div class="sign">
+    <div id="sign" class="sign">
       <ul class="sign__element">
+        <li v-if="isLoggedIn !== true" class="sign__element-signup">
+          <router-link to="/signup"
+            ><button class="sign__elements-login btn-signup">
+              Sign Up
+            </button></router-link
+          >
+        </li>
         <li v-if="isLoggedIn !== true" class="sign__element-login">
           <router-link to="/login"
             ><button class="sign__elements-login btn-login">
               Log In
-            </button></router-link
-          >
-        </li>
-        <li v-if="isLoggedIn !== true" class="sign__element-login-signup">
-          <router-link to="/signup"
-            ><button class="sign__elements-login btn-signup">
-              Sign Up
             </button></router-link
           >
         </li>
@@ -51,38 +82,6 @@
       </ul>
     </div>
   </div>
-
-  <footer class="footer">
-    <div class="footer__logos">
-      <img
-        class="footer__logos-behelp"
-        src="../assets/logos/Logo_wb_orange.svg"
-        alt="logo"
-        height="53.02px"
-      />
-      <div>
-        <img
-          class="footer__logos-hyf"
-          src="../assets/hyf_logo.png"
-          alt="HYF"
-          height="53.02px"
-        />
-      </div>
-    </div>
-
-    <div class="footer__text">
-      <p class="footer__text-p">
-        Project from the students of HYF <br />
-        info@behelp.be <br />
-        1000 Bruxelles
-      </p>
-    </div>
-    <div class="footer__icons">
-      <div class="fa-home"></div>
-      <a href="#"><img src="../assets/fas/twitter.svg" class="fa" /></a>
-      <a href="#"><img src="../assets/fas/instagram.svg" class="fa" /></a>
-    </div>
-  </footer>
 </template>
 <style lang="scss">
 @import "../components/styles/abstract/_variables.scss";
