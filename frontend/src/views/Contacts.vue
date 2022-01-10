c2236e9df3afddda7046bd324523b9894a634f29
TatsianaRud <tatsianaRud86@gmail.com>
2022-01-10 20:58:56 +0100

    first v

--- frontend/src/views/Contacts.vue
+++ frontend/src/views/Contacts.vue
@@ -1,7 +1,87 @@
 <template>
-  <div>
-    <h1>Contact</h1>
-    <p>Contacts part of the application</p>
-  </div>
+  
+    <div class="contactUs">
+      <div class="contact__content">
+         <div class="contactUs__text"><h1>Do you have questions?<br> Contact Us:</h1></div>
+    <div class="contactUs__img"><img src="../assets/img/5d8d941e933874bb0b403ffc5ac47e08.jpeg" alt="img"></div>
+      </div>
+     
+    
+     <div class="contactUs__page">
+       <p class="contact__text">We want to hear you! 
+         <br>
+         <br>
+
+         Kindly reach out to us via our CONTACT FORM or any of our media channels below
+</p>
+
+        <form class="form" @submit.prevent="sendEmail">
+         
+         
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+         
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            required
+            />
+         
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+     </div>
+    </div>
+  
 </template>
-<style></style>
+
+<script>
+import emailjs from 'emailjs-com';
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('service_lzyi0s6', 'template_y7zrvwv', e.target, 'user_6RSzucdIUsj0oQzdwj3VZ', {
+          name: this.name,
+          email: this.email,
+          message: this.message
+        })
+        alert("Message sended")
+      } catch (error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import '../components/styles/abstract/_variables.scss';
+@import '../components/styles/layout/_contact.scss';
+</style>
