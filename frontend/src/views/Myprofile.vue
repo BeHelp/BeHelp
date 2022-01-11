e777bc32e7c733a0a971925c6f90c9bc2feb3a20
danielhalasz <halaszdaniel@gmail.com>
2022-01-11 22:24:03 +0100

    Daniel

--- frontend/src/views/Myprofile.vue
+++ frontend/src/views/Myprofile.vue
@@ -3,13 +3,6 @@ import Profile from "../components/Profile.vue";
 
 export default {
   components: { Profile },
-
-  mounted() {
-    const plugin = document.createElement("script");
-    plugin.setAttribute("src", "../src/assets/js/cloudinary.js");
-    plugin.async = true;
-    document.head.appendChild(plugin);
-  },
 };
 </script>
 
