25992af076432dba264a5112383f7a946ee3678f
danielhalasz <halaszdaniel@gmail.com>
2022-01-14 00:41:12 +0100

    searchresults cards style fixed

--- frontend/src/views/Volunteers.vue
+++ frontend/src/views/Volunteers.vue
@@ -20,20 +20,21 @@ export default {
       <Search />
       <div class="volunteers__container">
         <div class="volunteers__searchresults">
-          <div class="volunteers__searchresults-h1">Results:</div>
-          <pre id="json">
-          <!-- {{ this.results }} -->
-          
-        </pre>
-          <Card
-            v-for="volunteer in searchResult"
-            v-bind:volunteer="volunteer"
-          />
+          <div class="volunteers__searchresults-h1"></div>
+
+          <div class="cards__columns">
+            <Card
+              v-for="volunteer in searchResult"
+              v-bind:volunteer="volunteer"
+            />
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <style lang="scss">
 @import "../components/styles/abstract/_variables.scss";
+@import "../components/styles/layout/_card.scss";
 </style>
