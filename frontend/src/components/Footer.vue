1b7228d3a118a8661186630a569b99db19652a24
danielhalasz <halaszdaniel@gmail.com>
2022-01-14 11:14:39 +0100

    correcting elements because of footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -16,19 +16,76 @@
       />
     </div>
 
-    <div class="footer__text">
-      <p class="footer__text-p">
-        {{ $t("hyfstudents") }}<br />
-        info@behelp.be <br />
-        1000 Bruxelles
-      </p>
-    </div>
-    <div class="footer__icons">
-      <a href="#"><img src="../assets/fas/facebook.svg" class="fa" /></a>
-      <a href="#"><img src="../assets/fas/twitter.svg" class="fa" /></a>
-      <a href="#"><img src="../assets/fas/instagram.svg" class="fa" /></a>
+    <div class="footer__cont2">
+      <div class="footer__cont2-text">
+        <p>
+          Project from the students of HYF <br />
+          info@behelp.be <br />
+          1000 Bruxelles
+        </p>
+      </div>
+      <div class="footer__cont2-icons">
+        <a href="#"><i class="bx bxl-facebook"></i></a>
+        <!-- <a href="#"><img src="../assets/fas/facebook.svg" class="fa" /></a> -->
+        <a href="#"> <i class="bx bxl-twitter"></i></a>
+        <!-- <a href="#"><img src="../assets/fas/twitter.svg" class="fa" /></a> -->
+        <a href="#"><i class="bx bxl-instagram"></i></a>
+        <!-- <a href="#"><img src="../assets/fas/instagram.svg" class="fa" /></a> -->
+      </div>
     </div>
   </footer>
+  <div>
+    <div id="sign" class="sign">
+      <ul class="sign__element">
+        <li v-if="isLoggedIn !== true" class="sign__element-signup">
+          <router-link to="/signup"
+            ><button class="sign__elements-login btn-signup">
+              Sign Up
+            </button></router-link
+          >
+        </li>
+        <li v-if="isLoggedIn !== true" class="sign__element-login">
+          <router-link to="/login"
+            ><button class="sign__elements-login btn-login">
+              Log In
+            </button></router-link
+          >
+        </li>
+
+        <li
+          v-if="isLoggedIn === true"
+          @click="hidden = !hidden"
+          class="footer__nav-features"
+        >
+          <p class="footer__nav-features p-username" id="header-username">
+            {{ user.firstName }}
+          </p>
+        </li>
+        <li v-if="isLoggedIn === true" class="footer__nav-features usermenu">
+          <img
+            @click="hidden = !hidden"
+            :src="user.photoURL"
+            class="footer__nav-features img-username"
+          />
+          <div
+            id="usermenu-list"
+            class="usermenu-list"
+            :class="{ 'foo-hover': hidden }"
+          >
+            <router-link
+              to="/myprofile"
+              class="usermenu-a"
+              id="usermenu-profile"
+              >My profile</router-link
+            ><br />
+            <a @click="logout" href="#" class="usermenu-a" id="usermenu-logout"
+              >Logout</a
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <style lang="scss">
 @import "../components/styles/abstract/_variables.scss";
