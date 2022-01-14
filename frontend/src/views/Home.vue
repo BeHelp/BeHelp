66fc87a58fe4df67bc706dac646421e2a7130578
danielhalasz <halaszdaniel@gmail.com>
2022-01-14 09:56:54 +0100

    mobile & tablet header final

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,9 +8,9 @@ export default {
   name: "Home",
   components: {
     Search,
-    Whoarewe,
-    Services,
-    Testimony,
+    // Whoarewe,
+    // Services,
+    // Testimony,
   },
 };
 </script>
